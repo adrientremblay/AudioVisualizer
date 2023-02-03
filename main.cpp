50e5d49daee53973e09c95371a0a4b94bca77406
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-02 19:59:13 -0500

    Open audio interface

--- main.cpp
+++ main.cpp
@@ -1,47 +1,43 @@
-/*
- *  This extra small demo sends a random samples to your speakers.
- */
 #include <alsa/asoundlib.h>
+#include <iostream>
 
-static char *device = "default";            /* playback device */
-unsigned char buffer[16*1024];              /* some random data */
-int main(void)
-{
-    int err;
-    unsigned int i;
-    snd_pcm_t *handle;
-    snd_pcm_sframes_t frames;
-    for (i = 0; i < sizeof(buffer); i++)
-        buffer[i] = random() & 0xff;
-    if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
-        printf("Playback open error: %s\n", snd_strerror(err));
-        exit(EXIT_FAILURE);
+struct audio_data_t {
+    double *cava_in;
+
+    int input_buffer_size;
+    int cava_buffer_size;
+
+    int format;
+    unsigned int rate;
+    unsigned int channels;
+    char *source;  // alsa device, fifo path or pulse source
+    int im;        // input mode alsa, fifo, pulse, portaudio, shmem or sndio
+    int terminate; // shared variable used to terminate audio thread
+    char error_message[1024];
+    int samples_counter;
+    int IEEE_FLOAT;
+    pthread_mutex_t lock;
+};
+
+int main() {
+    int error;
+    struct audio_data_t* audio_data;
+    snd_pcm_t *capture_handle;
+    snd_pcm_hw_params_t *hw_params;
+    snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
+    snd_pcm_uframes_t buffer_size;
+    snd_pcm_uframes_t period_size;
+    snd_pcm_uframes_t frames = audio_data->input_buffer_size / 2;
+
+    // todo: what is hw:0?
+    error = snd_pcm_open(&capture_handle, "hw:0", SND_PCM_STREAM_CAPTURE, 0);
+
+    if (error) {
+        std::cerr << "Failure opening audio interface!" << std::endl;
+        return 0;
     }
-    if ((err = snd_pcm_set_params(handle,
-                                  SND_PCM_FORMAT_U8,
-                                  SND_PCM_ACCESS_RW_INTERLEAVED,
-                                  1,
-                                  48000,
-                                  1,
-                                  500000)) < 0) {   /* 0.5sec */
-        printf("Playback open error: %s\n", snd_strerror(err));
-        exit(EXIT_FAILURE);
-    }
-    for (i = 0; i < 16; i++) {
-        frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
-        if (frames < 0)
-            frames = snd_pcm_recover(handle, frames, 0);
-        if (frames < 0) {
-            printf("snd_pcm_writei failed: %s\n", snd_strerror(frames));
-            break;
-        }
-        if (frames > 0 && frames < (long)sizeof(buffer))
-            printf("Short write (expected %li, wrote %li)\n", (long)sizeof(buffer), frames);
-    }
-    /* pass the remaining samples, otherwise they're dropped in close */
-    err = snd_pcm_drain(handle);
-    if (err < 0)
-        printf("snd_pcm_drain failed: %s\n", snd_strerror(err));
-    snd_pcm_close(handle);
+
+    std::cout << "Audio interface opened!" << std::endl;
+
     return 0;
 }
