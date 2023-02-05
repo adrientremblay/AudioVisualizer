e49d500ed3d35160a4cbd659b0edfcb839a783b9
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-05 13:35:52 -0500

    Basic SFML project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    if (!sf::SoundBufferRecorder::isAvailable()) {
+        std::cerr << "Yo the sound buffer recorder isn't available... fix it." << std::endl;
+        return -1;
+    }
+
+    sf::
 
     while (window.isOpen()) {
         sf::Event event;
