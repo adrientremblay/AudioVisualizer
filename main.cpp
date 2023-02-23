700f8f067ec3a2ada75c48b4f1227a046465b949
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-23 13:00:54 -0500

    Add a scaling factor for bar height

--- main.cpp
+++ main.cpp
@@ -2,16 +2,12 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include <cmath>
-#include <complex>
-#include <fftw3.h>
 #include <mutex>
 #include "../include/FFTStream.h"
 
-std::vector<std::complex<double>> discreteFourierTransform(std::vector<std::complex<double>> x);
-void processSignal(sf::SoundBuffer soundBuffer);
-
 constexpr unsigned int WINDOW_WIDTH = 1024;
 constexpr unsigned int WINDOW_HEIGHT = 700;
+constexpr unsigned int BAR_HEIGHT_SCALING = 2;
 
 std::mutex mtx;
 
@@ -57,7 +53,7 @@ int main() {
 
             // todo: determine if this is necessary
 
-            float bar_height = abs(normalizedFrequencySpectrum[i]);
+            float bar_height = abs(normalizedFrequencySpectrum[i]) * BAR_HEIGHT_SCALING;
 
             rect.setSize(sf::Vector2f(WINDOW_WIDTH / FFTStream::CONSIDERATION_LENGTH, bar_height));
             rect.setPosition(sf::Vector2f(i * (WINDOW_WIDTH / FFTStream::CONSIDERATION_LENGTH), WINDOW_HEIGHT - bar_height));
@@ -69,70 +65,3 @@ int main() {
     }
     return 0;
 }
-
-/*
-
-void processSignal(sf::SoundBuffer soundBuffer) {
-    // The samples are in time domain, they represent a sequence of amplitudes -> the position of the speaker cone
-    const sf::Int16* samples = soundBuffer.getSamples();
-    std::size_t N = soundBuffer.getSampleCount();
-    //buffer.saveToFile("my_record.ogg");
-
-    double* fftw_in  = fftw_alloc_real(N);
-
-    for (int i = 0 ; i < N ; i++) {
-        fftw_in[i] = static_cast<double>(samples[i]);
-    }
-
-    fftw_complex* fftw_out  = fftw_alloc_complex(N);
-
-    // todo: window function
-
-    fftw_plan plan = fftw_plan_dft_r2c_1d(N, fftw_in, fftw_out, FFTW_ESTIMATE);
-    fftw_execute(plan);
-
-    // Calculating magnitudes
-    std::cout << "==============================" << std::endl;
-    for (int i = 0 ; i < N / 2 ; i++) {
-        double real = fftw_out[i][0];
-        double imag = fftw_out[i][1];
-        fftw_out[i][0] = sqrt(real * real + imag * imag);
-        fftw_out[i][1] = 0;
-        std:: cout << fftw_out[i][0] << std::endl;
-    }
-    std::cout << "==============================" << std::endl;
-
-    fftw_destroy_plan(plan);
-    fftw_free(fftw_out);
-
-}
-
-std::vector<std::complex<double>> discreteFourierTransform(std::vector<std::complex<double>> x) {
-    // Determine number of samples
-    int N = x.size();
-
-    // Allocate memory for X
-    std::vector<std::complex<double>> X;
-    X.reserve(N);
-
-    // Allocate memory for internals
-    std::complex<double> intSum;
-
-    // Looping
-    for (int k = 0 ; k < N ; k++) {
-        intSum = std::complex<double>(0, 0);
-
-        for (int n = 0 ; n < N ; n++) {
-            double inside = ((2*M_PI) / N) * k * n;
-            double realPart = cos(inside);
-            double imagPart = sin(inside);
-            std::complex<double> w(realPart, -imagPart);
-            intSum += x[n] * w;
-        }
-
-        X.push_back(intSum);
-    }
-
-    return X;
-}
- */
