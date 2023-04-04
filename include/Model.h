03ec330d5e39705803ea64e6491c62bf83913004
Adrien Tremblay <adrien@adrientremblay.com>
2023-04-04 16:32:33 -0400

    Finish Model.cpp implementation

--- include/Model.h
+++ include/Model.h
@@ -16,6 +16,7 @@ private:
     // model data
     std::vector<Mesh> meshes;
     std::string directory;
+    std::vector<Texture> textures_loaded;
 
     void loadModel(std::string path);
     void processNode(aiNode *node, const aiScene *scene);
